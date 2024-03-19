3811e85087122d2497504be13974c93a68cd8d40
saikatCV <saikat030703@gmail.com>
2024-03-19 22:31:22 +0530

    based upoun the value it execute the switch case and determine the wage for ether full time or part time employee

---
+++ EmployeeWage2/SolvingUsingSwitch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage2
+{
+    public  class SolvingUsingSwitch
+    {
+        public void switchImplementing()
+        {
+            int isparttime = 0;
+            int isfulltime = 1;
+            int employeerateperhour = 20;
+            int employeeWorkinghour;
+            int employeeWage;
+            Random random = new Random();
+            int employeecheck = random.Next(0, 2);
+            switch(employeecheck)
+            {
+                case 0:
+                    employeeWorkinghour=4;
+                    break;
+                case 1:
+                    employeeWorkinghour=8;
+                    break;
+                default:
+                    employeeWorkinghour = 0;
+                    break;
+
+            }
+            employeeWage = employeeWorkinghour * employeerateperhour;
+            Console.WriteLine("Employee wage  is :" + employeeWage);
+
+
+
+
+        }
+    }
+}
