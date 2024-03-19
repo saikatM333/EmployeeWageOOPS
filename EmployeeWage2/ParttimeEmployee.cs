983477a264696bbcdc883377ca42f2e5404a33ca
saikatCV <saikat030703@gmail.com>
2024-03-19 22:27:02 +0530

    comput e the wage for the part time employee

---
+++ EmployeeWage2/ParttimeEmployee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage2
+{
+    public  class ParttimeEmployee
+    {
+        public void PartimeEmployeeWage()
+
+        {
+            int isparttime = 0;
+            int isfulltime = 1;
+            int employeerateperhour = 20;
+            int employeeWorkinghour;
+            int employeeWage;
+            Random random = new Random();
+            int employeecheck = random.Next(0,2);
+
+            if (employeecheck == isparttime) 
+            {
+                employeeWorkinghour = 4;
+            }
+            else if (employeecheck == isfulltime)
+            {
+                employeeWorkinghour = 8;
+            }
+            else
+            {
+                employeeWorkinghour = 0;
+            }
+
+            employeeWage = employeeWorkinghour * employeerateperhour;
+            Console.WriteLine("Employee wage  is :" + employeeWage);
+
+
+        }
+    }
+}
