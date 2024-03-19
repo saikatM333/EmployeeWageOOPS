6005bf309329d91e4bc0ce09af71f9280a5528c2
saikatCV <saikat030703@gmail.com>
2024-03-19 22:36:56 +0530

    compute the wages of the multiple companies

---
+++ EmployeeWage2/multipleWage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage2
+{
+    public class multipleWage
+    {
+        public void computeMultipleCompanyWage( string company , int empRatePerHour, int numOfWorkingDays, int MaxHoursPerMounth)
+        {
+            int employeeWorkinghour = 0, totalEmployeeHrs  = 0 , totalWorking=0;
+
+            while (totalEmpHrs<=MaxHoursPerMounth) {
+                totalWorking++;
+                Random random = new Random();
+                int employeecheck = random.Next(0, 2);
+                switch (employeecheck)
+                {
+                    case 0:
+                        employeeWorkinghour = 4;
+                        break;
+                    case 1:
+                        employeeWorkinghour = 8;
+                        break;
+                    default:
+                        employeeWorkinghour = 0;
+                        break;
+
+                }
+                totalEmployeeHrs += employeeWorkinghour;
+
+            }
+            int totalEmployeeWage = totalEmployeeHrs * empRatePerHour;
+
+            Console.WriteLine("total wage  is :" + totalEmployeeWage);
+
+
+
+
+
+
+
+
+        }
+    }
+}
