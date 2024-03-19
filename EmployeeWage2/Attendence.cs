ecf2ad2e9a6540e968dd35bb66c93eff120cb92c
saikatCV <saikat030703@gmail.com>
2024-03-19 22:21:22 +0530

    UC1 : generate the present or absent using random object

---
+++ EmployeeWage2/Attendence.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage2
+{
+    public  class Attendence
+    {
+        public  void attendenceGiver() {
+            int isfulltime = 1;
+            Random random = new Random();
+            int check = random.Next(0, 2);
+            if (check == 0)
+            {
+                Console.WriteLine("Employee is present "); 
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent ");
+
+            }
+
+                }
+    }
+}
