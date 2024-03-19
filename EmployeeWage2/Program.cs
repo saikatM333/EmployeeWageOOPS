ecf9121577da2a034818e11547bebc6c652381f1
saikatCV <saikat030703@gmail.com>
2024-03-19 22:19:39 +0530

    the main progrm file for calling diffrent class

---
+++ EmployeeWage2/Program.cs
@@ -0,0 +1,50 @@
+﻿using EmployeeWage2;
+
+public class Program
+{
+    public  static void Main(string[] args)
+    {
+        Console.WriteLine("employee wages ");
+
+        Console.WriteLine("press 0-7 to use diffrent functionality");
+
+        int choice =Convert.ToInt32(Console.ReadLine());
+        switch (choice)
+        {
+            case 0: Console.WriteLine("attenddence of employee ");
+                 Attendence attendence=  new Attendence();
+                attendence.attendenceGiver();
+                break;
+
+                case 1: Console.WriteLine("calculate the daily wage ");
+                  dailyWage wage = new dailyWage();
+                wage.dailyWageCalculater();
+                break;
+                case 2: Console.WriteLine("calculate the wage for the part time employee  ");
+                ParttimeEmployee parttimeEmployee = new ParttimeEmployee();
+                parttimeEmployee.PartimeEmployeeWage();
+                break;
+                case 3: Console.WriteLine("calculate the wage based on the type of employment ");
+                SolvingUsingSwitch solvingUsingSwitch = new SolvingUsingSwitch();
+                solvingUsingSwitch.switchImplementing();
+                break;
+                case 4: Console.WriteLine("calculate the mounthly wage ");
+                MounthlyWage  mounthly = new MounthlyWage();
+                mounthly.MounthlyWageCalculater();
+                break;
+                case 5: Console.WriteLine("calculate the wage based on the number of hours workedor  number of days worked ");
+                CoinditionalWage coinditionalWage = new CoinditionalWage();
+                coinditionalWage.CoinditionalWageCalculator();
+                break;
+                case 6: Console.WriteLine("calculate the wage for the multiple companies ");
+                multipleWage company = new multipleWage();
+                company.computeMultipleCompanyWage("sai", 20, 20, 300);
+
+                break;
+                case 7: Console.WriteLine("calculate the total wage of the companies ");
+                TotalWage totalWage = new TotalWage("sai", 20, 20, 300);
+                totalWage.computeWage();
+                break;
+        }
+    }
+}
